c39c454256a7135d913f5ffc20a5077d1d166cc7
boolean <s.boolean@gmail.com>
2010-12-28 16:59:42 -0300

    added tags cloud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_filter :tags
+
   rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
     render :text => exception, :status => 500
   end
   protect_from_forgery
+
+  private
+
+  def tags
+    @tags = Nota.tag_counts_on(:tags)
+  end
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 require 'textilizer'
 module ApplicationHelper
+  include ActsAsTaggableOn::TagsHelper
+
   def textilize(text)
     Textilizer.new(text).to_html.html_safe unless text.blank?
   end
-  
+
 end
+
