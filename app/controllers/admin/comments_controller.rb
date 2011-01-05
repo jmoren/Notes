9f767a0e890d295bf50d195ab0d171a378b517be
Fernando Saenz <s.boolean@gmail.com>
2011-01-05 15:55:41 -0300

    edit comments from administration module

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,8 +1,19 @@
 class Admin::CommentsController < AdminController
-  before_filter :find_comment, :only => [:destroy]
+  before_filter :find_comment, :only => [:edit, :update, :destroy]
 
   def index
     @comments = Comment.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @comment.update_attributes(params[:comment])
+      redirect_to admin_comments_path, :notice => 'Comment was updated!'
+    else
+      render 'edit'
+    end
   end
 
   def destroy
